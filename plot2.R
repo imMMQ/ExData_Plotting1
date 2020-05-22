cc5cf57ec9a826360226abe66f881f61438d2f00
imMMQ <ruiwang.im@gmail.com>
2020-05-22 13:16:18 -0500

    this is the second plot of the project.

---
+++ plot2.R
@@ -0,0 +1,21 @@
+powerdata <- read.table("household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";")
+TimenDate <- strptime(paste(powerdata$Date, powerdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+powerdata <- cbind(powerdata, TimenDate)
+
+powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
+powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power, na.rm = TRUE)
+powerdata$Global_reactive_power <- as.numeric(powerdata$Global_reactive_power, na.rm = TRUE)
+powerdata$Voltage <- as.numeric(powerdata$Voltage, na.rm = TRUE)
+powerdata$Global_intensity <- as.numeric(powerdata$Global_intensity, na.rm = TRUE)
+powerdata$Sub_metering_1 <- as.numeric(powerdata$Sub_metering_1, na.rm = TRUE)
+powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2, na.rm = TRUE)
+powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3, na.rm = TRUE)
+
+subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
+png("plot2.png", width=480, height=480)
+plot(subsetdata$TimenDate, subsetdata$Global_active_power, xlab = " ", ylab = "Global active power (kilowatts)", type = "l")
+dev.off()
+
+
+
